0ade1d62d3cb74cec839ed34cb9bf337fe566e9b
alexsandrosr88 <alexsandrosr@msn.com>
2023-01-07 04:29:50 -0300

    Projeto-sds4

---
+++ projeto-sds4/frontend/src/utils/formats.ts
@@ -0,0 +1,17 @@
+import{ format } from 'date-fns';
+
+export const round = (value: number, precision: number) => {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
+export const formatLocalDate = (date: string, pattern: string) => {
+    const dt = new Date(date);
+    const dtDateOnly = new Date(dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000);
+    return format(dtDateOnly, pattern);
+}
+
+export const formatter = new Intl.NumberFormat('pt-BR',{
+    style: 'currency',
+    currency: 'BRL'
+})
